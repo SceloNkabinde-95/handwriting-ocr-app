03327bcdfbac1a23fc84ed8de3531bcde24db9cd
SceloNkabinde-95 <scelonka@gmail.com>
2025-07-05 19:53:39 +0200

    Insert frontend connection in backend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Allow frontend to connect
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:5173","http://localhost:3000"],  # <-- frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
